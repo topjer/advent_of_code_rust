13a7a71c3d2c3b1064c21f5fa51d314d8253a70e
eb205 <christopher.schmaeche@deutsche-boerse.com>
2023-12-18 21:43:46 +0100

    Clean up code

--- src/solutions/year_2023/day_17.rs
+++ src/solutions/year_2023/day_17.rs
@@ -5,9 +5,9 @@ pub fn solve() {
     let lines = read_file("./src/inputs/year_2023/day_17");
 
     println!("Day 1");
-    let result_part_1 = logic_part_1(&lines);
+    let result_part_1 = logic_part_1(&lines, (0,3));
     println!("The result of the first part is: {}", result_part_1);
-    let result_part_2 = logic_part_2(&lines);
+    let result_part_2 = logic_part_1(&lines, (3, 10));
     println!("Outcome of the second part is: {}", result_part_2) ;
     println!("------------");
 }
@@ -21,7 +21,6 @@ fn parse_input(input: &Vec<String>) -> Vec<Vec<usize>> {
         });
         res.push(holder);
     }
-    //println!("{:?}", res);
     res
 }
 
@@ -51,63 +50,37 @@ struct Path {
 fn get_heat(map: &Vec<Vec<usize>>, point: &Point ) -> usize {
     map[point.row][point.column]
 }
-fn possible_directions2(path: &Path) -> Vec<Direction> {
+
+fn possible_directions(path: &Path, min_straight_till_turn: usize, max_straight: usize) -> Vec<Direction> {
     let mut possible_directions: Vec<Direction> = Vec::new();
     match path.last_step {
         Direction::Right => {
-            if path.straights > 3 {
+            if path.straights > min_straight_till_turn {
                 possible_directions.push(Direction::Up);
                 possible_directions.push(Direction::Down);
             }
-            if path.straights < 10 {possible_directions.push(Direction::Right)}
+            if path.straights < max_straight {possible_directions.push(Direction::Right)}
         },
         Direction::Left => {
-            if path.straights > 3 {
-            possible_directions.push(Direction::Up);
-            possible_directions.push(Direction::Down);
+            if path.straights > min_straight_till_turn {
+                possible_directions.push(Direction::Up);
+                possible_directions.push(Direction::Down);
             }
-            if path.straights < 10 {possible_directions.push(Direction::Left)}
+            if path.straights < max_straight {possible_directions.push(Direction::Left)}
         },
         Direction::Up => {
-            if path.straights > 3 {
-            possible_directions.push(Direction::Left);
-            possible_directions.push(Direction::Right);
+            if path.straights > min_straight_till_turn {
+                possible_directions.push(Direction::Left);
+                possible_directions.push(Direction::Right);
             }
-            if path.straights < 10 {possible_directions.push(Direction::Up)}
+            if path.straights < max_straight {possible_directions.push(Direction::Up)}
         },
         Direction::Down => {
-            if path.straights > 3 {
-            possible_directions.push(Direction::Left);
-            possible_directions.push(Direction::Right);
+            if path.straights > min_straight_till_turn {
+                possible_directions.push(Direction::Left);
+                possible_directions.push(Direction::Right);
             }
-            if path.straights < 10 {possible_directions.push(Direction::Down)}
-        },
-    }
-    possible_directions
-}
-
-fn possible_directions(path: &Path) -> Vec<Direction> {
-    let mut possible_directions: Vec<Direction> = Vec::new();
-    match path.last_step {
-        Direction::Right => {
-            possible_directions.push(Direction::Up);
-            possible_directions.push(Direction::Down);
-            if path.straights < 3 {possible_directions.push(Direction::Right)}
-        },
-        Direction::Left => {
-            possible_directions.push(Direction::Up);
-            possible_directions.push(Direction::Down);
-            if path.straights < 3 {possible_directions.push(Direction::Left)}
-        },
-        Direction::Up => {
-            possible_directions.push(Direction::Left);
-            possible_directions.push(Direction::Right);
-            if path.straights < 3 {possible_directions.push(Direction::Up)}
-        },
-        Direction::Down => {
-            possible_directions.push(Direction::Left);
-            possible_directions.push(Direction::Right);
-            if path.straights < 3 {possible_directions.push(Direction::Down)}
+            if path.straights < max_straight {possible_directions.push(Direction::Down)}
         },
     }
     possible_directions
@@ -155,119 +128,38 @@ fn print_path(map: &Vec<Vec<usize>>, path: &Path) {
     }
 }
 
-fn logic_part_1 (input: &Vec<String>) -> usize {
+fn logic_part_1 (input: &Vec<String>, params: (usize, usize)) -> usize {
     let input_map = parse_input(input);
+    // initialization
+    // 
     let mut paths_to_check: HashMap<usize, Vec<Path>> = HashMap::new();
     let mut shortest_lenghts: HashMap<(Point, Direction, usize), usize> = HashMap::new();
-    let mut target_heat: usize = 1;
-    let p1 = Point{row: 0, column: 1};
-    let p2 = Point{row: 1, column: 0};
-    let h1 = get_heat(&input_map, &p1);
-    let h2 = get_heat(&input_map, &p2);
-    shortest_lenghts.insert((p1, Direction::Right, 1), h1);
-    shortest_lenghts.insert((p2, Direction::Down, 1), h1);
-    paths_to_check.insert(h1, vec![Path{
-        total_heat: h1,
+    let mut target_heat: usize = 0;
+    paths_to_check.insert(0, vec![Path{
+        total_heat: 0,
         last_step: Direction::Right,
-        current_position: p1,
-        straights: 1,
-        total_path: vec![Point{row:0, column: 0}, p1]
-    }]);
-    paths_to_check.insert(h2, vec![Path{
-        total_heat: h2,
-        last_step: Direction::Down,
-        current_position: p2,
-        straights: 1,
-        total_path: vec![Point{row:0, column: 0}, p2]
-    }]);
-    while paths_to_check.len() > 0 {
-        
-        if !paths_to_check.contains_key(&target_heat) || paths_to_check.get(&target_heat).unwrap().len() == 0 {
-            target_heat += 1;
-            continue;
-        }
-
-        let point_to_check = paths_to_check.get_mut(&target_heat).unwrap().pop().unwrap();
-        if point_to_check.current_position == (Point{row: input_map.len()-1, column: input_map[0].len()-1}) {
-            println!("{:?}", point_to_check);
-            print_path(&input_map, &point_to_check);
-            //println!("{:?}", point_to_check.total_heat);
-            return point_to_check.total_heat;
-        }
-
-        let possible_directions = possible_directions(&point_to_check);
-        let allowed_points = allowed_points(&input_map, &point_to_check, possible_directions);
-        for p in allowed_points {
-            let mut new_straights = 1;
-            if p.0 == point_to_check.last_step {
-                new_straights = point_to_check.straights + 1;
-            };
-            let new_heat = point_to_check.total_heat + get_heat(&input_map, &p.1);
-            if shortest_lenghts.contains_key(&(p.1, p.0, new_straights)) {
-                continue;
-            } 
-            else {
-                shortest_lenghts.insert((p.1, p.0, new_straights), new_heat);
-            }
-            let mut old_path = point_to_check.total_path.clone();
-            old_path.push(p.1);
-            let new_path = Path {
-                    total_heat: new_heat,
-                    last_step: p.0,
-                    straights: new_straights,
-                    current_position: p.1,
-                    total_path: old_path
-            };
-            paths_to_check.entry(new_heat).and_modify(|el|el.push(new_path.clone()
-            )).or_insert(vec![new_path.clone()]);
-        }
+        current_position: Point { row: 0, column: 0 },
+        straights: 0,
+        total_path: vec![Point{row: 0, column: 0}]
     }
-    0
-    //println!("{:?}", shortest_lenghts);
-    //1
-}
-
-fn logic_part_2 (input: &Vec<String>) -> usize {
-    let input_map = parse_input(input);
-    let mut paths_to_check: HashMap<usize, Vec<Path>> = HashMap::new();
-    let mut shortest_lenghts: HashMap<(Point, Direction, usize), usize> = HashMap::new();
-    let mut target_heat: usize = 1;
-    let p1 = Point{row: 0, column: 1};
-    let p2 = Point{row: 1, column: 0};
-    let h1 = get_heat(&input_map, &p1);
-    let h2 = get_heat(&input_map, &p2);
-    shortest_lenghts.insert((p1, Direction::Right, 1), h1);
-    shortest_lenghts.insert((p2, Direction::Down, 1), h1);
-    paths_to_check.insert(h1, vec![Path{
-        total_heat: h1,
-        last_step: Direction::Right,
-        current_position: p1,
-        straights: 1,
-        total_path: vec![Point{row:0, column: 0}, p1]
-    }]);
-    paths_to_check.insert(h2, vec![Path{
-        total_heat: h2,
-        last_step: Direction::Down,
-        current_position: p2,
-        straights: 1,
-        total_path: vec![Point{row:0, column: 0}, p2]
-    }]);
+    ]);
+    // start of actual algorithm
     while paths_to_check.len() > 0 {
-        
+        // check whether our target heat has available paths, if not, increase target_heat
         if !paths_to_check.contains_key(&target_heat) || paths_to_check.get(&target_heat).unwrap().len() == 0 {
             target_heat += 1;
             continue;
         }
 
+        // get a path for the target heat and continue it
         let point_to_check = paths_to_check.get_mut(&target_heat).unwrap().pop().unwrap();
         if point_to_check.current_position == (Point{row: input_map.len()-1, column: input_map[0].len()-1}) {
             println!("{:?}", point_to_check);
             print_path(&input_map, &point_to_check);
-            println!("{:?}", point_to_check.total_heat);
             return point_to_check.total_heat;
         }
 
-        let possible_directions = possible_directions2(&point_to_check);
+        let possible_directions = possible_directions(&point_to_check, params.0, params.1);
         let allowed_points = allowed_points(&input_map, &point_to_check, possible_directions);
         for p in allowed_points {
             let mut new_straights = 1;
@@ -300,15 +192,14 @@ fn logic_part_2 (input: &Vec<String>) -> usize {
 #[test]
 fn test_example_input() {
     let lines = read_file("./src/inputs/year_2023/day_17_unit");
-    let result = logic_part_1(&lines);
+    let result = logic_part_1(&lines, (0,3));
     assert!(result == 102);
 }
-
 
 #[test]
 fn test_example2_input() {
     let lines = read_file("./src/inputs/year_2023/day_17_unit");
-    let result = logic_part_2(&lines);
+    let result = logic_part_1(&lines, (3, 10));
     assert!(result == 94);
 }
 
@@ -323,3 +214,14 @@ fn enum_test()  {
     println!("{:?}", foo);
     }
 
+#[test]
+fn dict_test() {
+    let mut foo: HashMap<usize, Vec<usize>> = HashMap::new();
+    let temp: Vec<usize> = Vec::new();
+    foo.insert(1, vec![10,20,30]);
+    foo.get_mut(&1).unwrap().pop().unwrap();
+    foo.get_mut(&1).unwrap().push(40);
+    foo.get_mut(&2).get_or_insert(&mut temp.clone()).push(50);
+    foo.entry(2).and_modify(|el| el.push(50)).or_insert(vec![50]);
+    println!("{:?}", foo);
+}
